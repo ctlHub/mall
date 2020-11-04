bcd99c435f51c2d3dc550979731ef3aea47a1d11
李重辰 <themaner@qq.com>
2020-11-04 16:46:53 +0800

    完善注释

--- mbg/src/main/java/com/mall/mbg/MpNameConvert.java
+++ mbg/src/main/java/com/mall/mbg/MpNameConvert.java
@@ -1,12 +1,14 @@
 package com.mall.mbg;
 
-import com.baomidou.mybatisplus.core.toolkit.ArrayUtils;
 import com.baomidou.mybatisplus.generator.config.INameConvert;
 import com.baomidou.mybatisplus.generator.config.po.TableField;
 import com.baomidou.mybatisplus.generator.config.po.TableInfo;
 import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 
 /**
+ * 表名和字段名转化，在生成model前将表名前的模块前缀去除
+ * 例如：u_biz_user ---> biz_user
+ *
  * @author 李重辰
  * @date 2020/11/1 19:41
  */
@@ -24,7 +26,9 @@ public class MpNameConvert implements INameConvert {
     return processName(field.getName());
   }
 
-  //下划线转驼峰
+  /**
+   * 下划线转驼峰
+   */
   private String processName(String name) {
     return NamingStrategy.underlineToCamel(name);
   }

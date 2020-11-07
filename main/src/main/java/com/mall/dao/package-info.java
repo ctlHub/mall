0be0abf654e26763b351018d5bc47990ea3332bc
tanghao <tanghao2yx@163.com>
2020-11-07 12:52:52 +0800

    工程代码清理，结构整理

--- main/src/main/java/com/mall/dao/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * 当前包用户存储开发者自定义的mapper接口
- * 请将对应的mapper文件存放在src.main.resource.dao目录下
- */
-package com.mall.dao;
---
+++ model/src/test/java/com/mall/domain/ModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mall.domain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ModelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

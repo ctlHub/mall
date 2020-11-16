6c92e55f3d25429e843a61146893e9c63638f756
tanghao <tanghao2yx@163.com>
2020-11-16 12:40:06 +0800

    配置creatTime、updateTime自动填充

---
+++ main/src/main/java/com/mall/component/MyMetaObjectHandler.java
@@ -0,0 +1,25 @@
+package com.mall.component;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+import java.time.LocalDateTime;
+
+/**
+ * @author tanghao
+ * @date 2020/11/16 12:28
+ */
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+  @Override
+  public void insertFill(MetaObject metaObject) {
+    this.strictInsertFill(metaObject, "createTime", () -> LocalDateTime.now(), LocalDateTime.class);
+  }
+
+  @Override
+  public void updateFill(MetaObject metaObject) {
+    this.strictUpdateFill(metaObject, "updateTime", () -> LocalDateTime.now(), LocalDateTime.class);
+  }
+}

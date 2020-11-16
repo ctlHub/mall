526ef530848b4ab981056d66e7e9ddf744168e8b
tanghao <tanghao2yx@163.com>
2020-11-16 17:07:21 +0800

    添加updateUserId和createUserId自动填充

--- main/src/main/java/com/mall/component/MyMetaObjectHandler.java
+++ main/src/main/java/com/mall/component/MyMetaObjectHandler.java
@@ -1,7 +1,9 @@
 package com.mall.component;
 
 import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.mall.model.BizUser;
 import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 
 import java.time.LocalDateTime;
@@ -16,10 +18,22 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
   @Override
   public void insertFill(MetaObject metaObject) {
     this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
+    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (principal instanceof BizUser) {
+      BizUser bizUser = (BizUser) principal;
+      this.strictInsertFill(metaObject, "createUserId", bizUser::getId, Long.class);
+    }
+    //setFieldValByName("createUserId", 111L, metaObject);
   }
 
   @Override
   public void updateFill(MetaObject metaObject) {
     this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
+    Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+    if (principal instanceof BizUser) {
+      BizUser bizUser = (BizUser) principal;
+      this.strictInsertFill(metaObject, "updateUserId", bizUser::getId, Long.class);
+    }
+    //setFieldValByName("updateUserId", 111L, metaObject);
   }
 }

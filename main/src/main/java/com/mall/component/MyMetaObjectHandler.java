95171d00181d53ccd400b1a8ef73e379f02a6dbb
tanghao <tanghao2yx@163.com>
2020-11-16 12:41:09 +0800

    配置creatTime、updateTime自动填充

--- main/src/main/java/com/mall/component/MyMetaObjectHandler.java
+++ main/src/main/java/com/mall/component/MyMetaObjectHandler.java
@@ -15,11 +15,11 @@ public class MyMetaObjectHandler implements MetaObjectHandler {
 
   @Override
   public void insertFill(MetaObject metaObject) {
-    this.strictInsertFill(metaObject, "createTime", () -> LocalDateTime.now(), LocalDateTime.class);
+    this.strictInsertFill(metaObject, "createTime", LocalDateTime::now, LocalDateTime.class);
   }
 
   @Override
   public void updateFill(MetaObject metaObject) {
-    this.strictUpdateFill(metaObject, "updateTime", () -> LocalDateTime.now(), LocalDateTime.class);
+    this.strictUpdateFill(metaObject, "updateTime", LocalDateTime::now, LocalDateTime.class);
   }
 }

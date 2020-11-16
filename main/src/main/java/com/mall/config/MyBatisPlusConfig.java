897cc6127c810a22cbda12b48fbe3ba4f2e6b4c0
tanghao <tanghao2yx@163.com>
2020-11-16 12:42:11 +0800

    乐观锁配置

---
+++ main/src/main/java/com/mall/config/MyBatisPlusConfig.java
@@ -0,0 +1,31 @@
+package com.mall.config;
+
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * @author tanghao
+ * @date 2020/11/14 9:39
+ */
+@Configuration
+@EnableTransactionManagement
+@MapperScan("com.mall.mapper")
+public class MyBatisPlusConfig {
+
+  /**
+   * 乐观锁配置
+   *
+   * @return
+   */
+  @Bean
+  public MybatisPlusInterceptor mybatisPlusInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+    return interceptor;
+  }
+
+}

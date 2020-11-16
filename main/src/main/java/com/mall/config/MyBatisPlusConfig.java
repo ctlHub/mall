e62e6eb5fc0a881e4f3c47ec85ce6f773def9e44
tanghao <tanghao2yx@163.com>
2020-11-16 17:08:21 +0800

    sql规范配置，多租户字段merchant_id配置

--- main/src/main/java/com/mall/config/MyBatisPlusConfig.java
+++ main/src/main/java/com/mall/config/MyBatisPlusConfig.java
@@ -1,10 +1,17 @@
 package com.mall.config;
 
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.handler.TenantLineHandler;
+import com.baomidou.mybatisplus.extension.plugins.inner.IllegalSQLInnerInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.OptimisticLockerInnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.TenantLineInnerInterceptor;
+import com.mall.model.BizUser;
+import net.sf.jsqlparser.expression.Expression;
+import net.sf.jsqlparser.expression.LongValue;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 /**
@@ -17,14 +24,44 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class MyBatisPlusConfig {
 
   /**
-   * 乐观锁配置
-   *
-   * @return
+   * 插件配置
    */
   @Bean
   public MybatisPlusInterceptor mybatisPlusInterceptor() {
     MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    //多租户
+    TenantLineInnerInterceptor tenantLineInnerInterceptor = new TenantLineInnerInterceptor();
+    tenantLineInnerInterceptor.setTenantLineHandler(new TenantLineHandler() {
+      @Override
+      public Expression getTenantId() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        if (principal instanceof BizUser) {
+          BizUser bizUser = (BizUser) principal;
+          return new LongValue(bizUser.getMerchantId());
+        }
+        return null;
+        //return new LongValue(1L);
+      }
+
+      @Override
+      public String getTenantIdColumn() {
+        return "merchant_id";
+      }
+    });
+    interceptor.addInnerInterceptor(tenantLineInnerInterceptor);
+    //乐观锁
     interceptor.addInnerInterceptor(new OptimisticLockerInnerInterceptor());
+    return interceptor;
+  }
+
+  /**
+   * sql性能规范配置,只针对开发环境与测试环境
+   */
+  @Bean
+  public MybatisPlusInterceptor illegalSqlInnerInterceptor() {
+    MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+    IllegalSQLInnerInterceptor illegalSqlInnerInterceptor = new IllegalSQLInnerInterceptor();
+    interceptor.addInnerInterceptor(illegalSqlInnerInterceptor);
     return interceptor;
   }
 
